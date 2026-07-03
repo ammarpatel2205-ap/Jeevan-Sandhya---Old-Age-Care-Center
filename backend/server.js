d248d6752b9002b03a5a4d2e3a7ea6eecd231e04
ammarpatel-30 <ammupatel112@gmail.com>
2026-07-03 11:24:03 +0530

    Serve frontend

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,9 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, "public")));
 import express from "express";
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
@@ -263,6 +269,9 @@ app.put('/api/staff/:id', async (req, res) => {
    SERVER
 ===================== */
 const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
